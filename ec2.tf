3abf21b74dabb7f3db9a581ef6ef2acda072d005
jagadish7-25 <kadapajag7@gmail.com>
2024-09-10 14:26:36 +0200

    terrafom sg added

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "allow_all" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  # ... other configuration ...
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "example" {
+  # ... other configuration ...
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
