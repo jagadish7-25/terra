ae6b9af32f6027c1b4fd4c2e1493277bb4398a50
jagadish7-25 <kadapajag7@gmail.com>
2024-09-10 14:52:43 +0200

    instance updated

--- ec2.tf
+++ ec2.tf
@@ -23,3 +23,16 @@ resource "aws_security_group" "allow-ssh" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+resource "aws_instance" "terra" {
+  ami                     = "ami-0b4f379183e5706b9"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  tags = {
+    Name = "terra"
+  }
+
+
+  
+}
