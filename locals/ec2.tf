af44f8e8dc721de9c16ef41c9e8eedf564cf1930
jagadish7-25 <kadapajag7@gmail.com>
2024-09-13 13:48:54 +0200

    instance-type modified

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "new_inst" {
   ami           = data.aws_ami.data_new.image_id
   count = length(var.instance_type)
-  instance_type = element(var.instance_type,count.index)
+  instance_type = var.instance_names[count.index] == "mysql" ? "t3.micro" : "t2.micro"
 
   tags = {
     Name = var.instance_names[count.index]
