d05da52662f5b6989c835dec4b4cdf6257b7631e
jagadish7-25 <kadapajag7@gmail.com>
2024-09-16 14:49:21 +0200

    files updated

--- prove/ec2.tf
+++ prove/ec2.tf
@@ -22,20 +22,44 @@ resource "aws_security_group" "allow-ssh" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+   ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_instance" "terra" {
   ami                     = "ami-0b4f379183e5706b9"
   instance_type           = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
-   provisioner "local-exec" {
-    command = "echo ${self.private_ip} >> private_ips.txt"
-  }
-
+ 
   tags = {
     Name = "terra"
   }
 
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+       "sudo yum install nginx -y",
+       "sudo systemctl start nginx",
+       "sudo rm -rf  /usr/share/nginx/html/*",
+       "sudo cp index.html /usr/share/nginx/html/"
+    ]
+  }
+
 
   
+}
+
+output "nginx" {
+  value = aws_instance.terra.public_ip
 }
