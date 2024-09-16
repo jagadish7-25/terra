76d55ecd2c9fa85277801f345b44cdca754b99bd
jagadish7-25 <kadapajag7@gmail.com>
2024-09-16 14:23:03 +0200

    terra provissioners

---
+++ prove/ec2.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "allow-ssh" {
+  name        = "allow-ssh"
+  description = "creating sg for terraform"
+
+  tags = {
+    Name = "all-sshh"
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_instance" "terra" {
+  ami                     = "ami-0b4f379183e5706b9"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+   provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt"
+  }
+
+  tags = {
+    Name = "terra"
+  }
+
+
+  
+}
