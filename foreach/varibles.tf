a7d017fe70bb5c48692aeaa2a12fb5ae13f1ed04
jagadish7-25 <kadapajag7@gmail.com>
2024-09-16 13:16:21 +0200

    foreach

---
+++ foreach/varibles.tf
@@ -0,0 +1,27 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0b4f379183e5706b9"
+}
+
+variable "instances" {
+    type = map
+    default = {
+        mysql = "t3.micro"
+        backend = " t2.micro"
+        frontend = "t2.micro"
+    }
+  
+}
+
+variable "port" {
+    type = number
+    default = "22"
+  
+}
+
+
+
+
+
+
+
