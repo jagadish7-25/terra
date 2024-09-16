cea5d678ab336c6e22c94ce8667d36d95fd4abd1
jagadish7-25 <kadapajag7@gmail.com>
2024-09-16 14:17:30 +0200

    modified

--- foreach/r53.tf
+++ foreach/r53.tf
@@ -4,6 +4,6 @@ resource "aws_route53_record" "new_route" {
   name    = each.key == "frontend" ? var.dname : "${each.key}.${var.dname}" 
   type    = "A"
   ttl     = 1
-  records = each.key == frontend ? [each.value.public_ip] : [each.value.private_ip]
+  records = each.key == "frontend" ? [each.value.public_ip] : [each.value.private_ip]
   allow_overwrite = true
 } 
