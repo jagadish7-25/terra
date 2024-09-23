beac9b2a5a827c543c74b180837d1ee75d560cc7
jagadish7-25 <kadapajag7@gmail.com>
2024-09-23 14:40:01 +0200

    vpc added

---
+++ vpc/vpc.tf
@@ -0,0 +1,38 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "172.31.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "expense-2"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block ="172.31.1.0/16"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "expense-2-public"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block ="172.31.2.0/16"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "expense-2-private"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block ="172.31.3.0/16"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "expense-2-public"
+  }
+}
