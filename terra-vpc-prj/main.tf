3e37d4166c62c1f0f198f7f9472c5a5f66fffdcd
jagadish7-25 <kadapajag7@gmail.com>
2024-09-24 15:43:56 +0200

    edied

--- terra-vpc-prj/main.tf
+++ terra-vpc-prj/main.tf
@@ -52,7 +52,7 @@ resource "aws_subnet" "private" {
   )
 }
 
-resource "aws_subnet" "" {
+resource "aws_subnet" "database" {
   count = length(var.database_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.database_subnet_cidrs[count.index]
