6c49e7e7d34ddd2f7e38127f48ac7c2f8379cb2e
jagadish7-25 <kadapajag7@gmail.com>
2024-09-25 15:58:27 +0200

    added

--- terra-vpc-prj/variables.tf
+++ terra-vpc-prj/variables.tf
@@ -72,3 +72,11 @@ variable "private_route_table_tags" {
 variable "database_route_table_tags" {
   default = {}
 }
+variable "is_peering_required" {
+    type = bool
+    default = false
+}
+
+variable "vpc_peering_tags" {
+    default = {}
+}
