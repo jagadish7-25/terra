5ad86c1bcb920aa1c5a01d73abdfda8631518e37
jagadish7-25 <kadapajag7@gmail.com>
2024-09-25 14:02:44 +0200

    modified errors

--- terra-vpc-prj/main.tf
+++ terra-vpc-prj/main.tf
@@ -126,19 +126,19 @@ resource "aws_route_table" "database" {
 
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0"
-  gateway_id = aws_internet_gateway.main.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
 }
  
  resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = "0.0.0.0"
+  destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.nat.id
 }
  
   resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
-  destination_cidr_block    = "0.0.0.0"
+  destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.nat.id
 }
  #associations Bit complicated task- we use loop here. we have asscociate in two availabilty zones.
