9de08c2389f907ca2ecf3196746ec2c1c9e8bcca
jagadish7-25 <kadapajag7@gmail.com>
2024-09-25 21:16:12 +0200

    added

--- test-vpc/data.tf
+++ test-vpc/data.tf
@@ -1,8 +1,8 @@
-data "aws_instance" "foo" {
-  instance_id = "ami-0b4f379183e5706b9"
+# data "aws_instance" "foo" {
+#   instance_id = "ami-0b4f379183e5706b9"
 
-  filter {
-    name   = "image-id"
-    values = ["ami-0b4f379183e5706b9"]
-  }
-}
+#   filter {
+#     name   = "image-id"
+#     values = ["ami-0b4f379183e5706b9"]
+#   }
+# }
