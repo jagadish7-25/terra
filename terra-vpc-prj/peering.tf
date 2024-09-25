ba45a293466b6a8c4fbe2a8833cd9ef2919bd360
jagadish7-25 <kadapajag7@gmail.com>
2024-09-25 21:03:01 +0200

    modified peering

--- terra-vpc-prj/peering.tf
+++ terra-vpc-prj/peering.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "peer" {
   count = var.is_peering_required ? 1 : 0
-  peer_vpc_id   = aws_vpc.bar.id
-  vpc_id        = aws_vpc.foo.id
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        =  data.aws_vpc.default.id
    auto_accept   = true
 
   tags = merge(
@@ -37,5 +37,5 @@ resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = data.aws_route_table.main.route_table_id
   destination_cidr_block    = var.cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer[count.index].id
 }
